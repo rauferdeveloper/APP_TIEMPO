696974d0a8fd0bd836e1673994c0c7ca391323b3
rauferdeveloper <rauletecolegio@gmail.com>
2018-02-09 20:28:56 +0100

    Temperatura por paises

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 window.onload=function(){
   ciudad = document.getElementById("ciudad");
-  buscar=document.getElementById("buscar")
+  buscar=document.getElementById("buscar");
+  paises = document.getElementById("paises");
+  codigoActual="AF";
+  //listaCreada=false;
+  anadirPaises();
+  //listaCreada=true;
   document.onkeydown=function(elEvento){
     var evento = window.event||elEvento;
     if(evento.keyCode==13){
@@ -13,11 +18,19 @@ window.onload=function(){
     mostrarTiempo();
       ciudad.value="";
   }
+  
+  paises.onchange=function(){
+    var indice = this.selectedIndex;
+
+    var opcionSeleccionada = this.options[indice];
+     texto= opcionSeleccionada.text;
+     buscarCodigo(texto); 
+
+  }
 }
 
 
-function mostrarTiempo() {
-  
+function mostrarTiempo() {  
   // Obtener la instancia del objeto XMLHttpRequest
   if(window.XMLHttpRequest) {
      peticionHttp = new XMLHttpRequest();
@@ -28,7 +41,7 @@ function mostrarTiempo() {
   // Preparamos la funcion de respuesta
   peticionHttp.onreadystatechange = muestraContenido;
   // Realizamos peticion HTTP
-  peticionHttp.open('GET', "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+ciudad.value+",es') and u='c'&format=json", true);
+  peticionHttp.open('GET', "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='"+ciudad.value+","+codigoActual+"') and u='c'&format=json", true);
   peticionHttp.send(null);
   function muestraContenido() {
     if(peticionHttp.readyState == 4) {
@@ -44,4 +57,80 @@ function mostrarTiempo() {
     }
   }
 }
-/*https://github.com/umpirsky/country-list para los paises hay que filtrar por los continentes */
+
+function crearLista(padre,texto){
+  opcion = document.createElement("option");
+  opcion.setAttribute("value",texto);
+  textoLista = document.createTextNode(texto);
+  opcion.appendChild(textoLista);
+  padre.appendChild(opcion);
+}
+
+function anadirPaises() {
+
+  // Obtener la instancia del objeto XMLHttpRequest
+  if(window.XMLHttpRequest) {
+    peticionHttp = new XMLHttpRequest();
+  }
+  else if(window.ActiveXObject) {
+    peticionHttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  // Preparamos la funcion de respuesta
+  peticionHttp.onreadystatechange = muestraContenido;
+  // Realizamos peticion HTTP
+  peticionHttp.open('GET', 'json/paises.json', true);
+  peticionHttp.send(null);
+  function muestraContenido() {
+    if(peticionHttp.readyState == 4) {
+      if(peticionHttp.status == 200) {
+        //Creamos el objeto de tipo JSON
+        var json = peticionHttp.responseText;
+        var objetoJson=eval("("+json+")"); //Con esto queremos que javascript lo entienda como un array
+        var valores=objetoJson; 
+        //Recorremos el array
+        for(var clave in valores){
+          nombre=valores[clave];
+
+        
+          crearLista(paises,nombre);
+
+        }
+      }
+    }
+  }
+}
+function buscarCodigo(nombrePais) {
+
+  // Obtener la instancia del objeto XMLHttpRequest
+  if(window.XMLHttpRequest) {
+    peticionHttp = new XMLHttpRequest();
+  }
+  else if(window.ActiveXObject) {
+    peticionHttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  // Preparamos la funcion de respuesta
+  peticionHttp.onreadystatechange = muestraContenido;
+  // Realizamos peticion HTTP
+  peticionHttp.open('GET', 'json/paises.json', true);
+  peticionHttp.send(null);
+  function muestraContenido() {
+    if(peticionHttp.readyState == 4) {
+      if(peticionHttp.status == 200) {
+        //Creamos el objeto de tipo JSON
+        var json = peticionHttp.responseText;
+        var objetoJson=eval("("+json+")"); //Con esto queremos que javascript lo entienda como un array
+        var valores=objetoJson; 
+        //Recorremos el array
+        for(var clave in valores){
+          nombre=valores[clave];
+          if(nombrePais==nombre){
+            codigoActual=clave;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+/*https://github.com/umpirsky/country-list para los paises */
