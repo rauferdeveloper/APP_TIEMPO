7353f398a932a91f384ee1240b602c131bb9e13d
rauferdeveloper <rauletecolegio@gmail.com>
2018-02-11 04:27:45 +0100

    arreglar la ubicacion de las imagenes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 window.onload=function(){
+  caja =document.getElementById("caja");
+  caja.style.width="100%";
+  caja.style.height="100%";
+  caja.style.backgroundColor="red";
+  caja.style.position="relative";
   ciudad = document.getElementById("ciudad");
+
   buscar=document.getElementById("buscar");
   paises = document.getElementById("paises");
   temperatura=document.getElementById("temperatura");
+  imagen=document.getElementById("imagen");
+
+  imagenes = new Array();
+
+  document.body.appendChild(imagen);
+  existe=false;
   codigoActual="AF";
   paisActual="Afghanistan";
+ 
   //listaCreada=false;
   anadirPaises();
   //listaCreada=true;
@@ -13,6 +26,7 @@ window.onload=function(){
     if(evento.keyCode==13){
       mostrarTiempo();
       ciudad.value="";
+
 
     }
   }
@@ -58,11 +72,60 @@ function mostrarTiempo() {
         //Obtenemos la raíz del JSON
         var query=objetoJson.query;
         
-        if(query.count==0||query.results.channel.location.country!=paisActual){
+        if(query.count==0||query.results.channel.location.country!=paisActual&&codigoActual!="EA"){
           temperatura.innerHTML = "No existe la ciudad";
-
+          imagen.src="";
+          imagen.style.width="";
+          imagen.style.height="";
+          if(existe){
+            for(var i=0; i < imagenes.length;i++){
+              caja.removeChild(imagenes[i]);
+              
+            }
+            existe=false;
+          }
+       
         }else{
+         
           temperatura.innerHTML = query.results.channel.item.condition.temp;
+
+          imagen.src="img/icons/"+query.results.channel.item.condition.code+".png";
+          imagen.style.width="5%";
+          imagen.style.height="5%";
+
+         
+          dias=query;
+          dias=query.results.channel.item.forecast;
+
+            if(!existe){
+              for(var i=0; i < dias.length;i++){
+
+                imagenes[i]=document.createElement("img");
+                imagenes[i].src="img/icons/"+dias[i].code+".png";
+                imagenes[i].style.width="5%";
+                imagenes[i].style.height="5%";
+                imagenes[i].style.position="relative";
+                imagenes[i].style.marginTop=parseInt(imagen.style.height)+5+"%";
+    
+                if(i==0){
+                  imagenes[i].style.left="0%";
+    
+                }else{
+                  imagenes[i].style.left=parseInt(imagenes[i-1].style.left)+parseInt(imagenes[i-1].style.width)+"%";
+    
+                }
+              
+                caja.appendChild(imagenes[i]);
+            }
+            existe=true;
+
+          }else{
+            for(var i=0; i < dias.length;i++){
+              imagenes[i].src="img/icons/"+dias[i].code+".png";
+            }
+
+          }
+              
 
         }
        
